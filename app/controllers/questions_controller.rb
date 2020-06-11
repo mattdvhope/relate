5aa5535cb060f800c67051df03e8ac532918e565
mattdvhope <mattagape@me.com>
2020-06-11 22:12:31 +0700

    More setup

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,14 @@
 class QuestionsController < ApplicationController
 
   def create
-    binding.pry
-  end
-
-  def update
-    
+    array = []
+    params["selected"].each {|sel| array.push(sel["choice"])  }
+    render json: 
+      {
+        :selections => array,
+        :code=>200, 
+        :message=>"Successful!!"
+      }
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :questions, only: [:create, :update]
+  # global options responder -> makes sure OPTION request for CORS endpoints work
+  match '*path', via: [:options], to: lambda {|_| [204, { 'Content-Type' => 'text/plain' }]}
+
+  resources :questions, only: [:create]
 
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
