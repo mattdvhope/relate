0f8e0fa098b4362b27781d40ee38f4a0a891cb95
mattdvhope <mattagape@me.com>
2020-06-12 08:50:41 +0700

    Added comment

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,16 @@
 class QuestionsController < ApplicationController
 
   def create
+
+    # must bring in Question to have a valid Choice
+
     array = []
     params["selected"].each {|sel| array.push(sel["choice"])  }
+
+    params["selected"].each do |sel|
+      Choice.create()
+    end
+
     render json: 
       {
         :selections => array,
