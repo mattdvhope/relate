c6c8b32a84e3340ef203eeecb4118f6474bb378c
mattdvhope <mattagape@me.com>
2020-06-29 17:22:25 +0700

    Working on user & survey question selections

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,14 @@
 class QuestionsController < ApplicationController
 
   def create
-
-    # must bring in Question to have a valid Choice 
-
-
     questions = []
     params["questions"].each do |question|
       questions.push(question)
 # binding.pry
     end
+
+    fb_id = params["fb_id"]
+    user = User.find_by_fb_id(fb_id)
 
     # params["selected"].each do |sel|
     #   Choice.create()
@@ -17,7 +16,8 @@ class QuestionsController < ApplicationController
 
     render json: 
       {
-        :selections => questions,
+        :questions => questions,
+        :user => user,
         :code=>200, 
         :message=>"Successful!!"
       }
