b7c8332a23c7111062de3c3e012ad0087fb97b36
Matt Malone <mattagape@me.com>
2021-02-10 14:38:37 +0700

    Set up db

--- db/migrate/20200620104320_add_fb_id_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddFbIdToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :fb_id, :string
-  end
-end
--- db/migrate/20200709131722_add_line_id_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddLineIdToUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :line_id, :string
-  end
-end
